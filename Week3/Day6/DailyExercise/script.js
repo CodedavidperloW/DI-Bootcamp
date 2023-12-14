4274de6144efce130ed6d84d79af02637616b0b8
Codedavidperlow <mrdavidperlow@gmail.com>
2023-12-14 20:54:14 +0200

    Stars

---
+++ Week3/Day6/DailyExercise/script.js
@@ -0,0 +1,25 @@
+// let stars = ["*","**","***","****","*****","******"]
+
+// for(let i=0;i<stars.length;i++){
+//     console.log(stars[i])
+// }
+
+
+// for (let i = 0; i<6; i++){
+//     myStars = '';
+//     for(let j =0 ; j<=i; j++){
+//         myStars += ' *'
+//     }
+//     console.log(myStars)
+// }
+
+
+//nested loops
+let myCoolStars
+for(let i=0; i<6; i++){
+    myCoolStars='';
+    for(let j=0; j<=i; j++){
+        myCoolStars+=' *'
+    }
+    console.log(myCoolStars)
+}
