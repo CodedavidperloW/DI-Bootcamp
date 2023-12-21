5c0d4c77047d8364e5eca8680b6026bf0443eb18
Codedavidperlow <mrdavidperlow@gmail.com>
2023-12-21 20:13:10 +0200

    Book Lists

--- Week4/Day4/ExerciseXP/script.js
+++ Week4/Day4/ExerciseXP/script.js
@@ -50,29 +50,93 @@
 //Exercise 3: Change the Navbar
 
 //1
-let newId = document.getElementById("navBar")
-let newId2 = newId.setAttribute("id", "SocialNetworkNavigation")
-console.log(newId)
-//2
-let newCreatedElement = document.createElement("li")
-newCreatedElement.innerText="Logout"
-console.log(newCreatedElement)
-//3
-//Lines 61-65 didn't work
-//First Attempt: Not Successful
-// let listedItems = document.getElementsByTagName("ul")
-// listedItems.appendChild(newCreatedElement)
-// console.log(listedItems)
+// let newId = document.getElementById("navBar")
+// let newId2 = newId.setAttribute("id", "SocialNetworkNavigation")
+// console.log(newId)
 
-//corrected continuation below
-let addTheLi = document.querySelector("#SocialNetworkNavigation ul")
-//Challenging, since I forgot that I changed the id :)
-addTheLi.appendChild(newCreatedElement)
-console.log(addTheLi)
+//2
+// let newCreatedElement = document.createElement("li")
+// newCreatedElement.innerText="Logout"
+// console.log(newCreatedElement)
+
+//3
+////////Lines 61-65 didn't work
+////////First Attempt: Not Successful
+//////// let listedItems = document.getElementsByTagName("ul")
+//////// listedItems.appendChild(newCreatedElement)
+//////// console.log(listedItems)
+
+////corrected continuation below
+// let addTheLi = document.querySelector("#SocialNetworkNavigation ul")
+// //Challenging, since I forgot that I changed the id :)
+// addTheLi.appendChild(newCreatedElement)
+// console.log(addTheLi)
 
 //4
-let firstUlValue = addTheLi.firstElementChild.textContent
-console.log(firstUlValue)
-let lastUlValue = addTheLi.lastElementChild.textContent
-console.log(lastUlValue)
+// let firstUlValue = addTheLi.firstElementChild.textContent
+// console.log(firstUlValue)
+// let lastUlValue = addTheLi.lastElementChild.textContent
+// console.log(lastUlValue)
 
+//Exercise 4: My Book List
+
+
+
+let newDiv = document.createElement("div")
+newDiv.classList.add("listBooks")
+
+console.log(newDiv)
+
+let allBooks = [
+    { title: "Not A Lone Solider", author: "David Perlow", image: "Pics/NaaSoldier.jpg", alreadyRead: true },
+    { title: "Bunnicula", author: "Deborah Howe", image: "Pics/Bunnicula.jpg", alreadyRead: true },
+    { title: "Red Badge of Courage", author: "Stephen Crane", image: "Pics/RB_Courage.jpg", alreadyRead: true },
+    { title: "Colin Powel", author: "Colin Powell", image: "Pics/Colin_Powell.webp", alreadyRead: true },
+    { title: "example-boy", author: "Boy", image: "Pics/exampleBoy.jpg", alreadyRead: false },
+    { title: "example-baby", author: "Baby", image: "Pics/exampleBaby.jpg", alreadyRead: false },
+];
+
+allBooks.forEach(book => {
+    //Now display the information..
+    //How? Console.log? How can I display it in HTML??
+    //!!Create Different Elements !!
+
+    let newBookInformation = document.createElement("p")
+    newBookInformation.textContent = `The title:${book.title}, written by:${book.author}, Already read: ${book.alreadyRead ? 'true' : 'false'}`;
+    newDiv.appendChild(newBookInformation)
+})
+
+
+//Now I need to make a table, so that the above information can be placed there
+
+let table = document.createElement("table")
+let headerRow = table.createTHead().insertRow(0);
+let titleHeader = headerRow.insertCell(0);
+let authorHeader = headerRow.insertCell(1);
+let imageHeader = headerRow.insertCell(2);
+titleHeader.textContent = "Title";
+authorHeader.textContent = "Author";
+imageHeader.textContent = "Image";
+
+allBooks.forEach(book => {
+    let row = table.insertRow();
+    let titleCell = row.insertCell(0);
+    titleCell.textContent = book.title;
+    let authorCell = row.insertCell(1);
+    authorCell.textContent = book.author;
+    let imageCell = row.insertCell(2);
+    let image = document.createElement("img");
+    image.src = book.image;
+    image.width = 100;
+    imageCell.appendChild(image);
+
+    if (book.alreadyRead) {
+        titleCell.style.color = "red";
+        authorCell.style.color = "red";
+        image.style.border = "2px solid red";
+
+    }
+})
+
+newDiv.appendChild(table)
+document.body.appendChild(newDiv);
