98b11e2e05438ce56ed6a0210f13b42ff01ba549
Codedavidperlow <mrdavidperlow@gmail.com>
2024-01-04 20:06:12 +0200

    Database

--- Week5/Day4/DailyExercise/script.js
+++ Week5/Day4/DailyExercise/script.js
@@ -9,14 +9,3 @@ event.target.value = checkIt
 })
 
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     let placeForLetters = document.getElementById('lettersInput');
-
-//     placeForLetters.addEventListener('input', function (event) {
-//         // Use a regular expression to filter out non-letter characters
-//         const sanitizedValue = event.target.value.replace(/[^a-zA-Z]/g, '');
-
-//         // Update the input field with the sanitized value
-//         event.target.value = sanitizedValue;
-//     });
-// });
